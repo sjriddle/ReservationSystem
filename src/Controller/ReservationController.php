c7d5bdbcc370cfed9f0620d9124a774936da274d
sriddle <sriddle@youniqueproducts.com>
2019-04-17 18:11:25 -0600

    Implemented Symfony PHP micro-framework

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sriddle
+ * Date: 4/17/19
+ * Time: 5:33 PM
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ReservationController
+{
+    /*
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index() {
+        return $this->render('templates/reserve/index.html.twig');
+    }
+}
