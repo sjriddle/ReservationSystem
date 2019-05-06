50b58e1b154ef4b71dfc1beb9eaead56c68f824d
sriddle <sriddle@youniqueproducts.com>
2019-05-05 20:19:31 -0600

    Created form for the creation of new reservations

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use App\Entity\Reserve;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class ReservationController extends Controller
 {
@@ -17,6 +22,22 @@ class ReservationController extends Controller
         $reservations = $this->getDoctrine()->getRepository(Reserve::class)->findAll();
 
         return $this->render('reserve/index.html.twig', array('reservations' => $reservations));
+    }
+
+    /**
+     * @Route("/reserve/new", name="reservation_new")
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $reservation = new Reserve();
+        $form = $this->createFormBuilder($reservation)
+            ->add('first_name', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control')))
+            ->add('last_name', TextType::class, array('required' => true, 'attr' => array('class' => 'form-control')))
+//            ->add('res_time', TimeType::class, array('required' => true, 'input' => 'timestamp', 'widget' => 'choice'))
+            ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+            ->getForm();
+
+        return $this->render('reserve/new.html.twig', array('form' => $form->createView()));
     }
 
     /**
