48fc3f5934d2dccab81b10882f44b4515f5ac028
sriddle <sriddle@youniqueproducts.com>
2019-06-13 22:04:25 -0600

    Worked on some js for form autofill

--- public/js/main.js
+++ public/js/main.js
@@ -19,3 +19,14 @@ function findId() {
     method: 'GET'
   }).then(res => window.location = `/reserve/edit/${id}`);
 }
+
+function autofillFormDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+  document.getElementById('form_res_date').value = today;
+  console.log(today)
+}
